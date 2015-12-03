1a76ab53c9e0c5baee58399f93ff483052637921
Cory Benfield <lukasaoz@gmail.com>
2015-12-03 08:57:00 +0000

    Validate the distribution of the Priority Tree

--- test/test_priority.py
+++ test/test_priority.py
@@ -7,6 +7,7 @@ Tests for the Priority trees
 """
 from __future__ import division
 
+import collections
 import itertools
 
 from hypothesis import given
@@ -65,3 +66,29 @@ class TestPriorityTreeOutput(object):
 
         for i in range(period * 20):
             assert next(p) == next(pattern), i
+
+    @given(STREAMS_AND_WEIGHTS)
+    def test_priority_tree_distribution(self, streams_and_weights):
+        """
+        Once a full period of repetition has been observed, each stream has
+        been emitted a number of times equal to its weight.
+        """
+        p = priority.PriorityTree()
+        weights = []
+
+        for stream, weight in streams_and_weights:
+            p.insert_stream(stream_id=stream, weight=weight)
+            weights.append(weight)
+
+        period = sum(weights)
+
+        # Pop off the first n elements, which will always be evenly
+        # distributed.
+        for _ in weights:
+            next(p)
+
+        count = collections.Counter(next(p) for _ in range(period))
+
+        assert len(count) == len(streams_and_weights)
+        for stream, weight in streams_and_weights:
+            count[stream] == weight
