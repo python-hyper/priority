3b86fdd646a4756569e23d4b794a4785fe71f908
Cory Benfield <lukasaoz@gmail.com>
2015-12-07 14:35:45 +0000

    Correctly export PriorityLoop exception.

--- src/priority/__init__.py
+++ src/priority/__init__.py
@@ -2,4 +2,4 @@
 """
 priority: HTTP/2 priority implementation for Python
 """
-from .priority import Stream, PriorityTree, DeadlockError  # noqa
+from .priority import Stream, PriorityTree, DeadlockError, PriorityLoop  # noqa
